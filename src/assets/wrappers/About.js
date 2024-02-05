4343818b63b901656bcbc8037625e5ab9ac34661
miawithcode <cuttontail@gmail.com>
2024-02-05 14:32:54 +0800

    style: about page

--- src/assets/wrappers/About.js
+++ src/assets/wrappers/About.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.nav``;
+const Wrapper = styled.div`
+  padding: 2rem;
+  h3 {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+  p {
+    line-height: 2;
+    color: var(--dark);
+    margin-top: 2rem;
+  }
+`;
 
 export default Wrapper;
