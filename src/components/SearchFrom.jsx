ae325ab25eee32263cf2bb7a3ed5653b13b4be7c
miawithcode <cuttontail@gmail.com>
2024-02-06 17:35:40 +0800

    feat: search

--- src/components/SearchFrom.jsx
+++ src/components/SearchFrom.jsx
@@ -1,7 +1,7 @@
 import { Form, useNavigation } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/SearchForm';
 
-const SearchFrom = () => {
+const SearchFrom = ({searchTerm}) => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   return (
@@ -12,6 +12,7 @@ const SearchFrom = () => {
           name="search"
           className="form-input"
           placeholder="Search cocktail"
+          defaultValue={searchTerm}
         />
         <button type="submit" className="btn" disabled={isSubmitting}>
           {isSubmitting ? 'searching...' : 'search'}
